04bdd241997ea51781a0f75402087c4d14be1ceb
Luxr <1078666705@qq.com>
2022-09-28 14:33:52 +0900

    商家详情页分类及内容联动的实现

--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="content">
     <div class="category">
-      <div class="category__item category__item--active">全部商品</div>
-      <div class="category__item">秒杀</div>
-      <div class="category__item">新鲜水果</div>
-      <div class="category__item">休闲食品</div>
-      <div class="category__item">时令蔬菜</div>
-      <div class="category__item">肉蛋家禽</div>
+      <div
+        :class="{'category__item': true, 'category__item--active': currentTab === item.tab}"
+        v-for="item in categories"
+        :key="item.name"
+        @click="() => handleCategoryClick(item.tab)"
+      >
+        {{ item.name }}
+      </div>
     </div>
     <div class="product">
-      <div class="product__item">
+      <div
+        class="product__item"
+        v-for="item in contentList"
+        :key="item._id"
+      >
         <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
         <div class="product__item__detail">
-          <h4 class="product__item__title">番茄250g/份</h4>
-          <p class="product__item__sales">月售10件</p>
+          <h4 class="product__item__title">{{ item.name }}</h4>
+          <p class="product__item__sales">月售 {{ item.sales }} 件</p>
           <p class="product__item__price">
-            <span class="product__item__yen">&yen;</span>33.6
-            <span class="product__item__origin">&yen;66.6</span>
+            <span class="product__item__yen">&yen;</span>{{ item.price }}
+            <span class="product__item__origin">&yen;{{ item.oldPrice }}</span>
           </p>
+        </div>
+        <div class="product__number">
+          <span class="product__number__minus">-</span>
+          0
+          <span class="product__number__plus">+</span>
         </div>
       </div>
     </div>
@@ -25,10 +36,48 @@
 </template>
 
 <script>
-export default {}
+import { reactive, toRefs } from 'vue'
+import { get } from '@/utils/request'
+
+export default {
+  name: 'Content',
+  setup () {
+    const categories = [{
+      name: '全部商品',
+      tab: 'all'
+    }, {
+      name: '秒杀',
+      tab: 'seckill'
+    }, {
+      name: '新鲜水果',
+      tab: 'fruit'
+    }]
+
+    const data = reactive({
+      currentTab: categories[0].tab,
+      contentList: []
+    })
+    const getContentData = async (tab) => {
+      const result = await get('/api/shop/1/products', { tab })
+      if (result?.errno === 0 && result?.data?.length) {
+        data.contentList = result.data
+      }
+    }
+    const handleCategoryClick = (tab) => {
+      getContentData(tab)
+      data.currentTab = tab
+    }
+    getContentData('all')
+    const { contentList, currentTab } = toRefs(data)
+    return { categories, currentTab, contentList, handleCategoryClick }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+
 .content {
   display: flex;
   position: absolute;
@@ -42,7 +91,7 @@ export default {}
   overflow-y: scroll;
   height: 100%;
   width: .76rem;
-  background: #F5F5F5;
+  background: $search-bgColor;
 
   &__item {
     line-height: .4rem;
@@ -51,7 +100,7 @@ export default {}
     color: #333;
 
     &--active {
-      background: #FFF;
+      background: $bgColor;
     }
   }
 }
@@ -61,10 +110,15 @@ export default {}
   flex: 1;
 
   &__item {
+    position: relative;
     display: flex;
     padding: .12rem 0;
     margin: 0 .16rem;
-    border-bottom: .01rem solid #F1F1F1;
+    border-bottom: .01rem solid $content-bgColor;
+
+    &__detail {
+      overflow: hidden;
+    }
 
     &__img {
       width: .68rem;
@@ -76,20 +130,21 @@ export default {}
       margin: 0;
       line-height: .2rem;
       font-size: .14rem;
-      color: #333;
+      color: $content-fontcolor;
+      @include ellipsis;
     }
 
     &__sales {
       margin: .06rem 0;
       font-size: .12rem;
-      color: #333;
+      color: $content-fontcolor;
     }
 
     &__price {
       margin: 0;
       line-height: .2rem;
       font-size: .14rem;
-      color: #E93B3B;
+      color: $hightlight-fontColor;
     }
 
     &__yen {
@@ -100,9 +155,38 @@ export default {}
       margin-left: .06rem;
       line-height: .2rem;
       font-size: .12rem;
-      color: #999;
+      color: $light-fontColor;
       text-decoration: line-through;
     }
+
+    .product__number {
+      position: absolute;
+      right: 0;
+      bottom: .12rem;
+
+      &__minus, &__plus {
+        display: inline-block;
+        width: .2rem;
+        height: .2rem;
+        line-height: .16rem;;
+        border-radius: 50%;
+        font-size: .2rem;
+        text-align: center;
+      }
+
+      &__minus {
+        border: .01rem solid $medium-fontColor;
+        color: $medium-fontColor;
+        margin-right: .05rem;
+      }
+
+      &__plus {
+        background: $btn-bgColor;
+        color: $bgColor;
+        margin-left: .05rem;
+      }
+    }
   }
+
 }
 </style>
--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -12,7 +12,7 @@
         />
       </div>
     </div>
-    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl" />
+    <ShopInfo :item="item" :hideBorder="true" v-show="item.imgUrl"/>
     <Content />
   </div>
 </template>
@@ -20,8 +20,8 @@
 <script>
 import { reactive, toRefs } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-import { get } from '@/utils/request'
-import ShopInfo from '@/components/ShopInfo'
+import { get } from '../../utils/request'
+import ShopInfo from '../../components/ShopInfo'
 import Content from './Content'
 
 // 获取当前商铺信息
@@ -35,10 +35,7 @@ const useShopInfoEffect = () => {
     }
   }
   const { item } = toRefs(data)
-  return {
-    item,
-    getItemData
-  }
+  return { item, getItemData }
 }
 
 // 点击回退逻辑
@@ -52,55 +49,40 @@ const useBackRouterEffect = () => {
 
 export default {
   name: 'Shop',
-  components: {
-    ShopInfo,
-    Content
-  },
-  setup () {
-    const {
-      item,
-      getItemData
-    } = useShopInfoEffect()
+  components: { ShopInfo, Content },
+  setup() {
+    const { item, getItemData } = useShopInfoEffect()
     const handleBackClick = useBackRouterEffect()
     getItemData()
-    return {
-      item,
-      handleBackClick
-    }
+    return { item, handleBackClick }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/style/viriables.scss';
-
+@import '../../style/viriables.scss';
 .wrapper {
   padding: 0 .18rem;
 }
-
 .search {
   display: flex;
   margin: .14rem 0 .04rem 0;
   line-height: .32rem;
-
   &__back {
     width: .3rem;
     font-size: .24rem;
     color: #B6B6B6;
   }
-
   &__content {
     display: flex;
     flex: 1;
     background: $search-bgColor;
     border-radius: .16rem;
-
     &__icon {
       width: .44rem;
       text-align: center;
       color: $search-fontColor;
     }
-
     &__input {
       display: block;
       width: 100%;
@@ -111,7 +93,6 @@ export default {
       height: .32rem;
       font-size: .14rem;
       color: $content-fontcolor;
-
       &::placeholder {
         color: $content-fontcolor;
       }
